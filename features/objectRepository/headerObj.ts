2b3d834d7b1fe288f0e2cc88ec56820bd79c4650
OLGALEB <45601343+OLGALEB@users.noreply.github.com>
2021-06-02 03:19:08 +0300

    Update headerObj.ts

--- features/objectRepository/headerObj.ts
+++ features/objectRepository/headerObj.ts
@@ -1,6 +1,6 @@
 import { by, element, ElementFinder } from "protractor";
 
 export class HeaderRepo {
-    //readonly titleByTag: ElementFinder = element(by.tagName('h1'));
+    
     readonly domLink: ElementFinder = element(by.xpath('///a[@class="b-main-navigation__link"]//span[contains(text(),"Дома и квартиры")]'));
 }
